2842e8415f5755e7bf743b3ea4650b4212e7bf0c
KK-Konbannha <katsuk.618@gmail.com>
2026-02-09 17:08:14 +0900

    init flake

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "nix-config";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+  let
+    system = "x86_64-linux";
+  in
+  {
+    nixosConfigurations.vm = nixpkgs.lib.nixosSystem {
+      inherit system;
+      modules = [
+        ./hosts/vm/configuration.nix
+      ];
+    };
+  };
+};
